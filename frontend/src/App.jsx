9d0f087829f9b27afaff02e0ff3f45ff613bf818
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2024-10-29 17:04:24 +0545

    specified Routes in react frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home, CreateBook, DeleteBook, EditBook, ShowBook } from './pages'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className="bg-red-400 text-white">
-        App
-      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books/create' element={<CreateBook />} />
+        <Route path='/books/details/:id' element={<ShowBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/delete/:id' element={<DeleteBook />} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export * from './Home'
+export * from './CreateBook'
+export * from './DeleteBook'
+export * from './EditBook'
+export * from './ShowBook'
